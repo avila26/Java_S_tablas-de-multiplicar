aeb81a7706feb7deee061c8046f1f0bf60dd8e2c
Yuseydi <pamelacamisan26@gmail.com>
2023-10-29 02:13:44 -0500

    Tablas de multiplicar

---
+++ script.js
@@ -0,0 +1,16 @@
+for (let i = 1; i < 13; i++) {
+  console.log( i + " * "+ 1 +" = "+ 1*i);
+  console.log( i + " * "+ 2 +" = "+ 2*i);
+  console.log( i + " * "+ 3 +" = "+ 3*i);
+  console.log( i + " * "+ 4 +" = "+ 4*i);   
+  console.log( i + " * "+ 5 +" = "+ 5*i);
+  console.log( i + " * "+ 6 +" = "+ 6*i);
+  console.log( i + " * "+ 7 +" = "+ 7*i);
+  console.log( i + " * "+ 8 +" = "+ 8*i);
+  console.log( i + " * "+ 9 +" = "+ 9*i);  
+  console.log( i + " * "+ 10 +" = "+ 10*i);
+  console.log( i + " * "+ 11 +" = "+ 11*i);  
+  console.log( i+ " * "+ 12 +" = "+ 12*i);
+  console.log("------------------")
+}
+
